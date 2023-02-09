f054039dfcbc983cad66ad61809a18056c413859
Ale <ale.surdu29@gmail.com>
2023-02-09 16:14:27 +0100

    Add operation and operate functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {return a + b;}
+function subtract(a, b) {return a - b;}
+function multiply(a, b) {return a * b;}
+function divide(a, b) {
+    if(b === 0) return null;
+    return a / b;
+}
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+': return add(a, b);
+        case '-': return subtract(a, b); 
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: alert(`${operator} is not an operator`); break;
+    }
+}
+
+const para = document.querySelector('p');
+para.textContent = `${operate('0', 2, 0)}`;
+
+// console.log(x);
+// console.log(divide(4, 2));
