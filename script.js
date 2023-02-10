cb580b4eff78243aa61bbea15fda232d1f2d4ff8
Ale <ale.surdu29@gmail.com>
2023-02-10 01:18:00 +0100

    Add event listeners for buttons

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const calculatorScreen = document.querySelector('.calculator-screen');
+const buttons = document.querySelectorAll('button');
+const deleteButton = document.querySelector('.delete');
+const deleteAllButton = document.querySelector('.delete-all');
+let displayValue;
+
+
+function pressButton(){
+        buttons.forEach(button => button.addEventListener('click', (e) => {
+            if(e.target.textContent !== 'AC' && e.target.textContent !== 'C') {
+                calculatorScreen.textContent += e.target.textContent;
+                displayValue = calculatorScreen.textContent;
+            }
+    }))
+}
+
+function deleteSingleCharacter(){
+    deleteButton.addEventListener('click', () => {
+        calculatorScreen.textContent = calculatorScreen.textContent.slice(0, -1);
+        displayValue = calculatorScreen.textContent;
+    })
+}
+
+function deleteAllCharacters(){
+    deleteAllButton.addEventListener('click', () => {
+        calculatorScreen.textContent = "";
+        displayValue = "";
+    })
+}
+
+pressButton();
+deleteSingleCharacter();
+deleteAllCharacters();
+
 function add(a, b) {return a + b;}
 function subtract(a, b) {return a - b;}
 function multiply(a, b) {return a * b;}
@@ -15,9 +49,3 @@ function operate(operator, a, b){
         default: alert(`${operator} is not an operator`); break;
     }
 }
-
-// const para = document.querySelector('p');
-// para.textContent = `${operate('0', 2, 0)}`;
-
-// console.log(x);
-// console.log(divide(4, 2));
